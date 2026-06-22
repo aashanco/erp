56af4dde835c78a449db17b675575be50baaa1fb
aashanco <support@aashan.co>
2026-06-22 11:46:43 -0500

    Fix Supabase URL in supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://vccvkavmxlmhftczzgir.supabase.co',
+  'https://vccvkavmxlmhtfczzgir.supabase.co',
   'sb_publishable_FzxwKIfYYVLZSPivt6vKRQ_JPj4OEN2'
 )
